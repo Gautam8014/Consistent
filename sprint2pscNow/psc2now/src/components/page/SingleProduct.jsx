419c1cf71011b7e4b2a063cfa3c5d222f1f607e4
Gautam8014 <flt.gautam@gmail.com>
2023-06-19 18:46:01 +0530

    sigleproduct

--- sprint2pscNow/psc2now/src/components/page/SingleProduct.jsx
+++ sprint2pscNow/psc2now/src/components/page/SingleProduct.jsx
@@ -1,7 +1,41 @@
 import React from 'react'
+import { useState, useEffect } from 'react'
+import {useParams} from "react-router-dom"
 
 export const SingleProduct = () => {
+  const[state, setState]=useState({})
+  const{id}=useParams();
+
+
+  console.log(state)
+
+  useEffect(()=>{
+      getData();
+  },[])
+
+
+  const getData= async()=>{
+
+      let url=`http://localhost:8080/products/${id}`
+ let res= await fetch(url);
+ let data= await res.json();
+ setState(data)
+
+
+  }
   return (
-    <div>SingleProduct</div>
+    <div>
+        {
+            
+                 <div >
+                    <img src={state.image} alt="" width={200} height={200} />
+                    <p>{state.price}</p>
+                    <h2>{state.id}</h2>
+                    <h4>{state.category}</h4>
+                 
+                </div>
+          
+        } 
+    </div>
   )
 }
